80cf035d9c5c0915a86d5be6931615d870f3531f
alihakankurt <kurtalihakan@outlook.com>
2026-08-05 04:40:57 +0300

    Move update/render loop to Game struct

--- src/game.rs
+++ src/game.rs
@@ -1,17 +1,63 @@
+use std::{
+    io,
+    time::{Duration, Instant},
+};
+
+use crossterm::{
+    cursor::{MoveToColumn, MoveToRow},
+    event, execute,
+};
+
 use crate::Grid;
 
 pub struct Game {
     pub grid: Grid,
+    pub fps: u32,
 }
 
 impl Game {
-    pub fn new(width: u32, height: u32) -> Self {
-        return Game {
-            grid: Grid::new(width, height),
-        };
+    pub fn new(width: u32, height: u32, fps: u32) -> Self {
+        let grid = Grid::new(width, height);
+        return Game { grid, fps };
     }
 
-    pub fn update(&mut self) {
+    pub fn run(&mut self, stdout: &mut io::Stdout) -> io::Result<()> {
+        let mut running = true;
+        let frame_time = Duration::from_secs_f64(1.0 / self.fps as f64);
+
+        execute!(stdout, MoveToRow(0), MoveToColumn(0))?;
+        self.render();
+
+        while running {
+            let start_time = Instant::now();
+
+            self.update();
+
+            execute!(stdout, MoveToRow(0), MoveToColumn(0))?;
+            self.render();
+
+            while event::poll(Duration::from_millis(0))? {
+                if let event::Event::Key(key_event) = event::read()? {
+                    match key_event.code {
+                        event::KeyCode::Esc => {
+                            running = false;
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            let current_frame_time = start_time.elapsed();
+            if current_frame_time < frame_time {
+                std::thread::sleep(frame_time - current_frame_time);
+            }
+        }
+
+        return Ok(());
+    }
+
+    fn update(&mut self) {
         for y in 0..self.grid.height {
             for x in 0..self.grid.width {
                 let state = self.grid.get(x, y) << 4 | self.grid.count(x, y);
@@ -24,7 +70,7 @@ impl Game {
         self.grid.update();
     }
 
-    pub fn render(&self) {
+    fn render(&self) {
         for y in 0..self.grid.height {
             for x in 0..self.grid.width {
                 let ch = match self.grid.get(x, y) {
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
 mod game;
 mod grid;
 
-use std::{
-    io::{self, stdout},
-    time::{Duration, Instant},
-};
-
 use crossterm::{
-    cursor::{MoveToColumn, MoveToRow},
-    event::{self, Event, KeyCode},
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
 pub use game::Game;
 pub use grid::Grid;
+use std::io;
 
 fn main() -> io::Result<()> {
-    let mut stdout = stdout();
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen)?;
+    enable_raw_mode()?;
 
     // TODO(@alihakankurt): Update target FPS, width & height etc. as command-line options.
-    const TARGET_FPS: f64 = 20.0;
-    let target_frame_time = Duration::from_secs_f64(1.0 / TARGET_FPS);
-
+    const TARGET_FPS: u32 = 20;
     let width = 10;
     let height = 10;
-    let mut game = Game::new(width, height);
+    let mut game = Game::new(width, height, TARGET_FPS);
 
     // TODO(@alihakankurt): Create a way to set initial game state, maybe read from file or through command-line.
     game.grid.set(1, 0);
@@ -34,41 +29,10 @@ fn main() -> io::Result<()> {
     game.grid.set(0, 2);
     game.grid.update();
 
-    execute!(stdout, EnterAlternateScreen)?;
-    enable_raw_mode()?;
-
-    execute!(stdout, MoveToRow(0), MoveToColumn(0))?;
-    game.render();
-
-    // TODO(@alihakankurt): Move this main loop into a method inside Game struct.
-    let mut running = true;
-    while running {
-        let start_time = Instant::now();
-
-        game.update();
-
-        execute!(stdout, MoveToRow(0), MoveToColumn(0))?;
-        game.render();
-
-        while event::poll(Duration::from_millis(0))? {
-            if let Event::Key(key_event) = event::read()? {
-                match key_event.code {
-                    KeyCode::Esc => {
-                        running = false;
-                        break;
-                    }
-                    _ => {}
-                }
-            }
-        }
-
-        let frame_time = start_time.elapsed();
-        if frame_time < target_frame_time {
-            std::thread::sleep(target_frame_time - frame_time);
-        }
-    }
+    game.run(&mut stdout)?;
 
     disable_raw_mode()?;
     execute!(stdout, LeaveAlternateScreen)?;
-    Ok(())
+
+    return Ok(());
 }

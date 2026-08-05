48629479f4d3dcc3a3e4a160a90d2e73d8521fc3
alihakankurt <kurtalihakan@outlook.com>
2026-08-05 06:00:55 +0300

    Add panic handler to exit correctly & check whether stdout is tty

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,41 @@ mod game;
 mod grid;
 
 use crossterm::{
+    cursor::{Hide, Show},
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
 pub use game::Game;
 pub use grid::Grid;
-use std::io;
+use std::{
+    io::{self, IsTerminal},
+    panic,
+    process::exit,
+};
+
+fn initialize_panic_handler() {
+    let original_hook = panic::take_hook();
+
+    panic::set_hook(Box::new(move |panic_info| {
+        let _ = disable_raw_mode();
+        let _ = execute!(io::stdout(), LeaveAlternateScreen, Show);
+
+        original_hook(panic_info);
+    }));
+}
 
 fn main() -> io::Result<()> {
     let mut stdout = io::stdout();
 
-    execute!(stdout, EnterAlternateScreen)?;
+    if !stdout.is_terminal() {
+        println!("program is not running on terminal, exiting...");
+        exit(1);
+    }
+
+    execute!(stdout, EnterAlternateScreen, Hide)?;
     enable_raw_mode()?;
+
+    initialize_panic_handler();
 
     // TODO(@alihakankurt): Update target FPS, width & height etc. as command-line options.
     const TARGET_FPS: u32 = 20;
@@ -27,8 +50,8 @@ fn main() -> io::Result<()> {
 
     game.run(&mut stdout)?;
 
-    disable_raw_mode()?;
-    execute!(stdout, LeaveAlternateScreen)?;
+    let _ = disable_raw_mode();
+    let _ = execute!(stdout, LeaveAlternateScreen, Show);
 
     return Ok(());
 }
